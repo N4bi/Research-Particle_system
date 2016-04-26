27ff0249d45453ab0690766cff0f0abd8f1a8ea9
N4bi <latorrem10@gmail.com>
2016-04-26 19:37:41 +0200

    some cleaning

--- Motor2D/GameManager.h
+++ Motor2D/GameManager.h
@@ -8,22 +8,10 @@
 #include "Gui.h"
 
 //Number to change the number of waves
-#define TOTALWAVES 2
-#define WAVETIME1 5.0f //120 = 2 minutes per wave in the future or some other game designish number
-#define WAVETIME2 50.0f
-#define	SIZE1X 1 // Changes number of zerglings per wave
-#define SIZE1Y 1
-#define	SIZE2X 10 // Changes number of zerglings per wave
-#define SIZE2Y 1
-#define	SIZE3X 5 // Changes number of zerglings per wave
-#define SIZE3Y 2
-#define TOTALUNITSALLWAVES (SIZE1X*SIZE1Y + SIZE2X*SIZE2Y + SIZE3X*SIZE2Y)
-#define ZERGLINGSCORE 20
 #define COMMANDCENTERPOSITION {1500, 2250}
 #define SIZEMARINESX 4
 #define SIZEMARINESY 2
 
-class GuiImage;
 using namespace std;
 
 //Class that manages the game. The win lose cycle, the points earned, the resources, waves etc..
