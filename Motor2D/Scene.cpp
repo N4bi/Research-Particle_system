bea677ff175a4c8c5d1b912fbe68865f879f3634
N4bi <latorrem10@gmail.com>
2016-04-29 16:52:07 +0200

    new explosion for test

--- Motor2D/Scene.cpp
+++ Motor2D/Scene.cpp
@@ -116,72 +116,78 @@ bool Scene::update(float dt)
 		float min = 0.0f;
 		float max = 360.0f;
 
+
 		Emisor* e = app->particle->addEmisor(p, m.x, m.y, emisorDuration, particleLife, 
 			particleVelocity, min, max, "Particles/Explosion/fire.png");
 	
 		e->speed.set(0, 0);
 	}
 
-	/*if (app->input->getKey(SDL_SCANCODE_J) == KEY_DOWN)
+	if (app->input->getKey(SDL_SCANCODE_J) == KEY_DOWN)
 	{
 		iPoint m;
 		app->input->getMousePosition(m);
 		m = app->render->screenToWorld(m.x, m.y);
 
 		Particle p;
-		p.anim.frames.push_back({ 0, 0, 80, 90 });
-		p.anim.frames.push_back({ 78, 0, 78, 156 });
-		p.anim.frames.push_back({ 156, 0, 78, 156 });
-		p.anim.frames.push_back({ 234, 0, 78, 156 });
-		p.anim.frames.push_back({ 312, 0, 78, 156 });
-		p.anim.frames.push_back({ 390, 0, 78, 156 });
-		p.anim.frames.push_back({ 468, 0, 78, 156 });
-		p.anim.frames.push_back({ 546, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.frames.push_back({ 624, 0, 78, 156 });
-		p.anim.speed = 0.08f;
-		p.anim.loop = true;
+		p.anim.frames.push_back({ 0, 0, 128, 128 });
+		p.anim.frames.push_back({ 128, 0, 128, 128 });
+		p.anim.frames.push_back({ 256, 0, 128, 128 });
+		p.anim.frames.push_back({ 384, 0, 128, 128 });
+		p.anim.frames.push_back({ 512, 0, 128, 128 });
+		p.anim.frames.push_back({ 640, 0, 128, 128 });
+		p.anim.frames.push_back({ 768, 0, 128, 128 });
+		p.anim.frames.push_back({ 896, 0, 128, 128 });
+		p.anim.frames.push_back({ 0, 128, 128, 128 });
+		p.anim.frames.push_back({ 128, 128, 128, 128 });
+		p.anim.frames.push_back({ 256, 128, 128, 128 });
+		p.anim.frames.push_back({ 384, 128, 128, 128 });
+		p.anim.frames.push_back({ 512, 128, 128, 128 });
+		p.anim.frames.push_back({ 640, 128, 128, 128 });
+		p.anim.frames.push_back({ 768, 128, 128, 128 });
+		p.anim.frames.push_back({ 896, 128, 128, 128 });
+		p.anim.frames.push_back({ 0, 256, 128, 128 });
+		p.anim.frames.push_back({ 128, 256, 128, 128 });
+		p.anim.frames.push_back({ 256, 256, 128, 128 });
+		p.anim.frames.push_back({ 384, 256, 128, 128 });
+		p.anim.frames.push_back({ 512, 256, 128, 128 });
+		p.anim.frames.push_back({ 640, 256, 128, 128 });
+		p.anim.frames.push_back({ 768, 256, 128, 128 });
+		p.anim.frames.push_back({ 896, 256, 128, 128 });
+		p.anim.frames.push_back({ 0, 384, 128, 128 });
+		p.anim.frames.push_back({ 128, 384, 128, 128 });
+		p.anim.frames.push_back({ 256, 384, 128, 128 });
+		p.anim.frames.push_back({ 384, 384, 128, 128 });
+		p.anim.frames.push_back({ 512, 384, 128, 128 });
+		p.anim.frames.push_back({ 640, 384, 128, 128 });
+		p.anim.frames.push_back({ 768, 384, 128, 128 });
+		p.anim.frames.push_back({ 896, 384, 128, 128 });
+		p.anim.frames.push_back({ 0, 512, 128, 128 });
+		p.anim.frames.push_back({ 128, 512, 128, 128 });
+		p.anim.frames.push_back({ 256, 512, 128, 128 });
+		p.anim.frames.push_back({ 384, 512, 128, 128 });
+		p.anim.frames.push_back({ 512, 512, 128, 128 });
+		p.anim.frames.push_back({ 640, 512, 128, 128 });
+		p.anim.frames.push_back({ 768, 512, 128, 128 });
+		p.anim.frames.push_back({ 896, 512, 128, 128 });
+		p.anim.speed = 0.01f;
+		p.anim.loop = false;
 		p.anim.current_frame = 0.0f;
 
 
 		float emisorDuration = 1.0f;
-		Uint32 particleLife = 2;
-		int particleVelocity = 40;
-		float min = 0.0f;
-		float max = 360.0f;
+		Uint32 particleLife = 5;
+		int particleVelocity = 500;
+		float min = 250.0f;
+		float max = 290.0f;
 
+	/*	particle = app->particle->addParticle(p, m.x, m.y, 5, "Particles/Explosion/explosion.png");
+*/
 		Emisor* e = app->particle->addEmisor(p, m.x, m.y, emisorDuration, particleLife,
-			particleVelocity, min, max, "Particles/Explosion/Small_Explosion.png");
+		particleVelocity, min, max, "Particles/Explosion/explosion.png");
 
-		e->speed.set(0, 0);
-	}*/
+		//e->speed.set(0, 0);
+	}
 
 	return true;
 }
